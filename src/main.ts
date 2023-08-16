a6a22354162ede118fb672d98fb3f30fbbc9ced5
StrangeNoob <sipun63427452@gmail.com>
2023-08-17 02:00:08 +0530

    add: some testcases

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,17 @@ async function bootstrap() {
       },
     }),
   );
+
+  // Set up Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Bitespeed API')
+    .setDescription('API documentation for Bitespeed')
+    .setVersion('1.0')
+    .addTag('bitespeed')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
